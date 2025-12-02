3eb2392ff3c41474f909130b2c5ceeaea1e1e4ba
Dexter617 <jordancolegio617@gmail.com>
2025-12-02 16:35:32 -0500

    Script BBDD
    Se carga la base de datos con poca información.

---
+++ Natlan/BBDD/Creacion BD Natlan.sql
@@ -0,0 +1,11 @@
+Select * from natlan.direccion;
+Select * from natlan.empleado;
+Select * from natlan.cliente;
+
+Select * from natlan.rol;
+
+Select * from natlan.estado;
+ALTER TABLE cliente
+MODIFY estado_id INT NULL;
+
+
